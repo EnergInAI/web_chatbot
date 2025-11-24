a9158446797f81308720cca7aa048d4460042fd0
EnergInAI™ <energinai.solutions@gmail.com>
2025-11-25 00:12:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
-GEMINI_API_KEY = "AIzaSyAXnTdA1xY4nwgzxOKevY_ovfuxr0QJ4yE"
+import os
 
-EMBED_MODEL = "models/text-embedding-004"
-CHAT_MODEL = "gemini-2.0-flash"
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+EMBED_MODEL = os.getenv("EMBED_MODEL", "models/text-embedding-004")
+CHAT_MODEL  = os.getenv("CHAT_MODEL", "gemini-2.0-flash")
 
-VECTOR_DB_PATH = "vector_store/faiss_index"
+VECTOR_DB_PATH = os.getenv("VECTOR_DB_PATH", "vector_store/faiss_index")
